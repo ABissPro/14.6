766a59e2806a0b6ed6783850a6a2bae181458833
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-02-23 01:11:32 +0300

    Add files via upload

---
+++ 4. Умножение матрицы на вектор.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+	system("chcp 1251"); // Установить кодировку
+	float a[4][4];
+	std::vector<float> b;
+	float c[4][4];
+
+	std::cout << "Введите числа матрицы\n";
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {			
+			std::cin >> a[i][j];
+		}		
+	}
+	std::cout << "Введите числа вектора\n";
+	for (int i = 0; i < 4; i++) {
+		float temp;
+		std::cin >> temp;
+		b.push_back(temp);
+	}
+
+	std::cout << "Результат:\n";
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			c[i][j] = a[i][j] * b[j];
+			std::cout << c[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
